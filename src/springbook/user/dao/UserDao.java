04ecc4a63ab02982accb679365fffbec045232d2
ctrl0703 <ctrl0703@naver.com>
2018-05-01 18:38:55 +0900

    change config file java -> xml

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -9,8 +9,8 @@ import springbook.user.domain.User;
 
 public class UserDao {
 	private ConnectionMaker connectionMaker;
-	
-	public UserDao(ConnectionMaker connectionMaker) {
+
+	public void setConnectionMaker(ConnectionMaker connectionMaker) {
 		this.connectionMaker = connectionMaker;
 	}
 	
