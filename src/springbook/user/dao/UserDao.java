c34bad6c1f0e1b153d2a5de0ae2e66c6aa5d9f66
ctrl0703 <ctrl0703@naver.com>
2018-05-05 11:55:21 +0900

    add exception code

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -59,29 +59,72 @@ public class UserDao {
 	}
 	
 	public void deleteAll() throws ClassNotFoundException, SQLException {
-		Connection c = dataSource.getConnection();
+		Connection c = null;
+		PreparedStatement ps = null;
 		
-		PreparedStatement ps = c.prepareStatement("DELETE FROM users");
-		
-		ps.executeUpdate();
-		
-		ps.close();
-		c.close();
+		try {
+			c = dataSource.getConnection();
+			ps = c.prepareStatement("DELETE FROM users");
+			ps.executeUpdate();
+		} catch(SQLException e) {
+			throw e;
+		} finally {
+			if(ps != null) {
+				try {
+					ps.close();
+				} catch(SQLException e) {
+					
+				}
+			}
+			if(c != null) {
+				try {
+					c.close();
+				} catch (SQLException e) {
+					
+				}
+			}
+		}
 	}
 	
 	public int getCount() throws ClassNotFoundException, SQLException {
-		Connection c = dataSource.getConnection();
+		Connection c = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		int result = 0;
 		
-		PreparedStatement ps = c.prepareStatement("SELECT COUNT(*) FROM users");
+		try {
+			c = dataSource.getConnection();
+			ps = c.prepareStatement("SELECT COUNT(*) FROM users");
+			rs = ps.executeQuery();
+			rs.next();
+			
+			result = rs.getInt(1);
+		} catch(SQLException e) {
+			throw e;
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch(SQLException e) {
+					
+				}
+			}
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch(SQLException e) {
+					
+				}
+			}
+			if (c != null) {
+				try {
+					c.close();
+				} catch(SQLException e) {
+					
+				}
+			}
+		} 
 		
-		ResultSet rs = ps.executeQuery();
-		rs.next();
-		int count = rs.getInt(1);
-		
-		rs.close();
-		ps.close();
-		c.close();	
-		
-		return count;
+		return result;
 	}
 }
