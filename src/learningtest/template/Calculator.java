f50af36d5b51263d8f65f7ca88d96928e2d5437a
ctrl0703 <ctrl0703@naver.com>
2018-05-05 14:31:03 +0900

    learning test(template/callback)

---
+++ src/learningtest/template/Calculator.java
@@ -0,0 +1,58 @@
+package learningtest.template;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Calculator {
+
+	public int calcSum(String filepath) throws IOException {
+		LineCallback<Integer> sumCallback = new LineCallback<Integer>() {
+			@Override
+			public Integer doSomethingWithLine(String line, Integer value) {
+				return value + Integer.valueOf(line);
+			}
+		};
+		return lineReadTemplate(filepath, sumCallback, 0);
+	}
+
+	public int calcMultiply(String filepath) throws IOException {
+		LineCallback<Integer> sumCallback = new LineCallback<Integer>() {
+			@Override
+			public Integer doSomethingWithLine(String line, Integer value) {
+				return value * Integer.valueOf(line);
+			}
+		};
+		return lineReadTemplate(filepath, sumCallback, 1);
+	}
+
+	public String concatenate(String filepath) throws IOException {
+		LineCallback<String> concatenateCallback = new LineCallback<String>() {
+			@Override
+			public String doSomethingWithLine(String line, String value) {
+				return value + Integer.valueOf(line);
+			}
+		};
+		return lineReadTemplate(filepath, concatenateCallback, "");
+	}
+
+	public <T> T lineReadTemplate(String filepath, LineCallback<T> callback, T initVal) throws IOException {
+		BufferedReader br = null;
+		
+		try {
+			br = new BufferedReader(new FileReader(filepath));
+			T result = initVal;
+			String line = null;
+			while((line = br.readLine()) != null) {
+				result = callback.doSomethingWithLine(line, result);
+			}
+			return result;
+		} catch(IOException e) {
+			System.out.println(e.getMessage());
+			throw e;
+		} finally {
+			if (br != null) {
+				try {br.close();} catch(IOException e) {System.out.println(e.getMessage());}
+			}
+		}
+	}
+}
